3a42ba6ac790b95a2bd5699eac9e53b09d5bd453
gOSMAN11 <64037855+gOSMAN11@users.noreply.github.com>
2020-04-20 20:56:30 +0200

    Update PizzaMenu.java

--- MariosPizzabar/src/PizzaMenu.java
+++ MariosPizzabar/src/PizzaMenu.java
@@ -23,13 +23,21 @@ public class PizzaMenu {
 
     // instancierer pizzaer og tilføjer dem til menu
     public void createMenu() {
-        Pizza test = new Pizza(5,"Test","tomatsauce, ost, skinke og oregano",57);
-        Pizza vesuvio = new Pizza(1,"Vesuvio","tomatsauce, ost, skinke og oregano",57);
-        Pizza amerikaner = new Pizza(2,"Amerikaner","tomatsauce, ost, skinke og oregano",57);
+        pizzaMenu.add(new Pizza(1, "Vesuvio", "tomatsauce, ost, skinke og oregano", 57));
+        pizzaMenu.add(new Pizza(2, "Amerikaner", "tomatsauce, ost, oksefars og oregano", 53));
+        pizzaMenu.add(new Pizza(3, "Caciatore", "tomatsauce, ost, peperoni og oregano", 57));
+        pizzaMenu.add(new Pizza(4, "Carbona", "tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano", 63));
+        pizzaMenu.add(new Pizza(5, "Dennis", "tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano", 65));
+        pizzaMenu.add(new Pizza(6, "Bertil", "tomatsauce, ost, skinke, champignon og oregano", 57));
+        pizzaMenu.add(new Pizza(7, "Silvia", "tomatsauce, ost, pepperoni, rød peber, løg, oliven og oregano", 61));
+        pizzaMenu.add(new Pizza(8, "Victoria", "tomatsauce, ost, skinke, ananas, champignon, løg og oregano", 61));
+        pizzaMenu.add(new Pizza(9, "Toronfo", "tomatsauce, ost, skinke, bacon, kebab, chili og oregano", 61));
+        pizzaMenu.add(new Pizza(10, "Capricciosa", "tomatsauce, ost, skinke, champignon og oregano", 61));
+        pizzaMenu.add(new Pizza(11, "Hawai", "tomatsauce, ost, skinke, ananas og oregano", 57));
+        pizzaMenu.add(new Pizza(12, "Le Blissola", "tomatsauce, ost, skinke, rejer og oregano", 61));
+        pizzaMenu.add(new Pizza(13, "Venezia", "tomatsauce, ost, skinke, bacon og oregano", 61));
+        pizzaMenu.add(new Pizza(14, "Mafia", "tomatsauce, ost, pepperoni, bacon, løg og oregano", 61));
 
-        pizzaMenu.add(test);
-        pizzaMenu.add(vesuvio);
-        pizzaMenu.add(amerikaner);
     }
 
     // tilføjer pizzaer til menu
